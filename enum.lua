72237c92c9951e03a85d4fd6b4d3a4c84e3bc21c
Skaruts <Skaruts@users.noreply.github.com>
2019-10-21 04:13:02 +0100

    Some improvements
    - improve code
    - remove name
    - rename 'max' to 'count'
    - add iterator 'items()'

--- enum.lua
+++ enum.lua
@@ -21,102 +21,91 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
 --==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==
-local ENUM_MT = {}
-ENUM_MT = {
-	__index = function(t, k) return ENUM_MT[k] end,
+local Enum = {}
+local MT = {
+	__index = function(t, k) return
+		t._fields[k] or Enum[k] or t._iterable_values[k]
+		or error(string.format("\n\n    Field %s does not exist in enum.\n\n", k), 2)
+	end,
+	__newindex = function(t, k, v) error("\n\n    Enum is immutable.\n\n", 2) end,
 	__tostring = function(t)
-		local str = ""
-		if t.name then str = t.name .. " {\n"
-		else           str = "enum {\n"
-		end
-		for i=1, t.max do
-			local idx = t._values[i]
-			local v = t._ordered_fields[idx]
-			str = string.format("%s    %-4d %s\n", str, idx, v)
+		local str = "{\n"
+		for i=1, #t._ordered_fields do
+			local k = t._ordered_fields[i]
+			local v = t._fields[k]
+			str = str.. string.format("    %-4d %s\n", v, k)
 		end
 		return str .. "}"
 	end
 }
-
--- used to check for duplicated keys
-local function is_in(t, new_k)
-	for k, v in pairs(t) do
-		if new_k == k then return true end
+function Enum.new(...)
+	if type(...) ~= "string" and type(...) ~= "table" then
+		error("\n\n    Invalid object for enum initialization. Must be a table or list of strings.\n\n")
 	end
-	return false
-end
+	local t = { count = {}, _fields = {}, _ordered_fields = {}, _iterable_values = {} }
+	setmetatable(t, MT)
 
-local function enum(list, name)
-	local t = {}
-	t.name = name
-	t.max = #list
+	local exp = false    -- exponential stepping
+	local step = 1       -- incremental step
+	local start = 1      -- starting value
+	local elems = {...}
+	if type(elems[1]) == "table" then elems = elems[1] end
 
-	-- handy for __tostring()
-	t._ordered_fields = {}
-	t._values = {}
-
-	-- default values
-	local exp = false
-	local step = 1
-	local start = 1
-
-	-- if 1st element contains enum formatting, parse it and remove it
-	if not string.match(list[1], "^[a-zA-Z_]+") then
-		local str = list[1]
-		table.remove(list, 1)
-		t.max = t.max - 1
+	-- if 1st field is the enum formatting, parse it and remove it
+	if not string.match(elems[1], "^[%a_]+") then
+		local str = elems[1]
+		table.remove(elems, 1)
 
 		-- if string begins with a number, set it as start
-		if tonumber(str:sub(1, 1)) ~= nil then
-			start = tonumber(string.match(str, "[0-9]+")) or 1
-		end
+		if str:match("^[%d-]") then start = tonumber(str:match("[%d-]+")) or 1 end
 
-		-- check if there's any '+' or '*'
-		local plus = string.find(str, '+')
-		local ast = string.find(str, '*')
-
-		if ast then
+		local plus = str:find('+')  -- check if there's any '+'
+		if str:find('*') then       -- or '*'
 			exp = true
-		elseif plus then
-			-- if a '+' exists, check if there's a custom increment
-			local inc = string.match(str:sub(plus+1, #str), "[0-9]+")
+		elseif plus then            -- if a '+' exists, check if there's a custom increment
+			local inc = string.match(str:sub(plus+1, #str), "[%d-]+")
 			if inc then step = inc end
 		end
 	end
 
-	local idx = start
-	for i=1, t.max do
-		-- try splitting the current entry into parts, if possible
-		local words = {}
-		for word in list[i]:gmatch("[a-zA-Z0-9_-]+") do table.insert(words, word) end
-		local k = words[1]
-		assert( not is_in(t, k), string.format("\n\n    Entry '%s' already exists in enum.\n\n", k) )
+	t.count = #elems
 
-		-- if a second element exists then current entry contains a
-		-- custom value; set 'idx' to it
-		if words[2] then
-			if idx == start then start = tonumber(words[2]) end -- if it's the 1st one, also set start to it
-			idx = tonumber(words[2])
+	local val = start
+	for i=1, #elems do
+		local words = {}    -- try splitting the current entry into parts, if possible
+		for word in elems[i]:gmatch("[%w_-]+") do
+			words[#words+1] = word
 		end
+
+		local k = words[1]
+		if t._fields[k] then error( string.format("\n\n    Field '%s' already exists in enum.\n\n", k) ) end
+
+		-- if a second element exists then current entry contains a custom value
+		if words[2] then val = tonumber(words[2]) end
 
 		-- store the entries and respective values
-		t[k] = idx
-		t._ordered_fields[idx] = k
-		table.insert(t._values, idx)
+		t._fields[k] = val
+		t._ordered_fields[i] = k    -- useful for printing
+		t._iterable_values[i] = val -- useful for iterators
 
-		-- increase 'idx' by increments or exponential growth
-		if not exp then
-			idx = idx + step
-		else
-			if idx < -1 and idx > 1 then
-				if idx < 0 then idx = math.ceil(idx/2)
-				else            idx = idx + idx end
-			else
-				idx = idx + 1
-			end
+		-- increase 'val' by increments or exponential growth
+		if not exp then                   val = val + step
+		elseif val < -1 or val > 1 then   val = val < 0 and math.ceil(val/2) or val + val
+		else                              val = val + 1
 		end
 	end
-	return setmetatable(t, ENUM_MT)
+	return t
 end
 
-return enum
+local function iterator(t, i)
+	i = i+1
+	local val = t[i]
+	if i > #t then return end
+	return i, val
+end
+
+function Enum:items()
+	return iterator, self._iterable_values, 0
+end
+
+return setmetatable( Enum, { __call = function(_, ...) return Enum.new(...) end } )
